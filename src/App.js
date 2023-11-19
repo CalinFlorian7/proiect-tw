5ba711f55aa147b6af5a90456205d7642b2ff23e
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-11-19 18:26:54 +0200

    ???

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import HelloMessage from "./components/helloMessage.js";
+import HelloMessage from './components/helloMessage.js'
 // import './App.css';
-
+import Navbar from './components./Navbar.js'
+import { Route, Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, route } from 'react-router-dom'
 function App() {
-  return (
-    <div className="App">
-    <HelloMessage />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <Router>
+                <Navbar></Navbar>
+                <Switch>
+                    <Route path="/"></Route>
+                </Switch>
+            </Router>
+            <HelloMessage /> */}
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+function Navbar() {
+    return (
+        <>
+            {/* <div className="navbar">
+                <link to="#" className="menu-bars">
+                    <FaIcons.FaBars />
+                </link>
+            </div> */}
+        </>
+    )
+}
+
+export default Navbar
