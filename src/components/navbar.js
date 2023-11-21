a147d5be0e361ffe5b16ad06a1c322f5dc9c2dcc
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-11-21 13:33:10 +0200

    css style for the menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,8 @@ import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 // import { IoIosClose } from 'react-icons/io'
-
+import { SidebarData } from './SidebarData'
+import './Navbar.css'
 function Navbar() {
     const [sidebar, setSidebar] = useState(false)
     const showSidebar = () => setSidebar(!sidebar)
@@ -21,6 +22,16 @@ function Navbar() {
                             <AiIcons.AiOutlineClose />
                         </Link>
                     </li>
+                    {SidebarData.map((item, index) => {
+                        return (
+                            <li key={index} className="item.cName">
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>{' '}
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         </>
