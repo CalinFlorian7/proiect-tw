f772496b396a48bf551c0ada18d188b6117f8fbe
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-11-21 14:01:46 +0200

    pages from the specified directory

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,35 +5,38 @@ import { Link } from 'react-router-dom'
 // import { IoIosClose } from 'react-icons/io'
 import { SidebarData } from './SidebarData'
 import './Navbar.css'
+import { IconContext } from 'react-icons'
 function Navbar() {
     const [sidebar, setSidebar] = useState(false)
     const showSidebar = () => setSidebar(!sidebar)
     return (
         <>
-            <div className="navbar">
-                <Link to="#" className="menu-bars">
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className="nav-menu-items">
-                    <li className="navbar-toggle">
-                        <Link to="#" className="menu-bars">
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
-                    {SidebarData.map((item, index) => {
-                        return (
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link>{' '}
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
+            <IconContext.Provider value={{ color: 'cyan' }}>
+                <div className="navbar">
+                    <Link to="#" className="menu-bars">
+                        <FaIcons.FaBars onClick={showSidebar} />
+                    </Link>
+                </div>
+                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className="nav-menu-items" onClick={showSidebar}>
+                        <li className="navbar-toggle">
+                            <Link to="#" className="menu-bars">
+                                <AiIcons.AiOutlineClose />
+                            </Link>
+                        </li>
+                        {SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>{' '}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </IconContext.Provider>
         </>
     )
 }
---
+++ src/pages/SignOff.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SignOff() {
+    return <div>SignOff</div>
+}
+
+export default SignOff
