6d07353f6e46306740f89542f955e29102a67af0
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-11-23 23:46:12 +0200

    default image set

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './UserMenu.css'
 import { RiArrowDownSFill } from 'react-icons/ri'
+import defalutImage from '../Images/defaultProfile.jpg'
 function UserMenu() {
     return (
         <div className="container-menu-components">
             <Link to="/Profile">
                 <div className="user-menu">
                     <span className="user-image">
-                        <img src="../components/gearrrrrrrr.png" alt=" " />
+                        <img src={defalutImage} alt=" " />
                     </span>
                     <span className="user-name">User Name</span>
                     <span className="user-arrow"></span>
