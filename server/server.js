95892983db26543366b96494523519e9ba51f833
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-12-21 19:22:08 +0200

    token generated

--- server/server.js
+++ server/server.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json())
 app.use('/api/teachers', TeacherRouter)
 app.use('/api/users', UserRouter)
 
+app.post('/api/login', (req, res) => {
+    const username = req.params.username
+    const user = { username: username }
+    jwt.sign(user, process.env.ACCESS_TOKEN)
+})
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`)
