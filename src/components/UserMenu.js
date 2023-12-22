c794a85f5e0f4b87c8c87751ac2ae9ee9a40d855
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-12-22 18:02:49 +0200

    user data fetched

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -5,7 +5,38 @@ import { RiArrowDownSFill } from 'react-icons/ri'
 
 import defalutImage from '../Images/defaultProfilePicture.jpg'
 function UserMenu() {
-    useEffect(() => {}, [])
+    useEffect(() => {
+        if (localStorage.getItem('userType') === 'student') {
+            const id = localStorage.getItem('userId')
+            const fechUserNameImage = async () => {
+                const response = await fetch(
+                    'http://localhost:3001/selectUserNameImage',
+                    {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ id: id }),
+                    }
+                ).catch((err) => {
+                    console.log(err)
+                })
+                const data = await response.json()
+                if (response.status === 500) {
+                    console.log('server error')
+                }
+                if (response.status === 200) {
+                    console.log(data)
+                    document.querySelector('.user-name').innerHTML =
+                        data[0].user_name
+                    if (data[0].user_image !== null)
+                        document.querySelector('.user-image img').src =
+                            data[0].user_image
+                }
+            }
+            fechUserNameImage()
+        }
+    }, [])
 
     return (
         <div className="container-menu-components">
