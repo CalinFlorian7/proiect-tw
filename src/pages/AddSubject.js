28c44e1f5bb05b5faf55d247ee823f042a02179e
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-12-24 20:00:06 +0200

    form for adding subject created

--- src/pages/AddSubject.js
+++ src/pages/AddSubject.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 function AddSubject() {
-    return <div>AddSubject</div>
+    return (
+        <>
+            <div className="page-container">
+                <div className="add-subject-container">
+                    <h1>Add a subject</h1>
+                    <form className="subject-form">
+                        <label htmlFor="subjectName">Subject Name:</label>
+                        <input
+                            type="text"
+                            id="subjectName"
+                            name="subjectName"
+                            placeholder="Enter subject name"
+                        />
+                        <button type="submit">Add Subject</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default AddSubject
