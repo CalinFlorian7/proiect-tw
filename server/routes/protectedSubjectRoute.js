7258d6b74087a40e04c816158fea34a155b92c94
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-12-25 18:01:44 +0200

    api added to router

--- server/routes/protectedSubjectRoute.js
+++ server/routes/protectedSubjectRoute.js
@@ -1,4 +1,5 @@
 const subjectController = require('../controllers/subjectController.js')
 const protectedRouter = require('express').Router()
 protectedRouter.post('/insertSubject', subjectController.insertSubject)
+protectedRouter.post('/selectSubjectIdName', subjectController.selectSubject)
 module.exports = protectedRouter
