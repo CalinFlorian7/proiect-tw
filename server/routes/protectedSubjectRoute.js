1241befa1afe2795e4bd16aa95f50b55f0a58dd3
RacaruCalin7 <racarucalinflorian@gmail.com>
2023-12-25 18:18:32 +0200

    subjects fetched from db

--- server/routes/protectedSubjectRoute.js
+++ server/routes/protectedSubjectRoute.js
@@ -1,5 +1,8 @@
 const subjectController = require('../controllers/subjectController.js')
 const protectedRouter = require('express').Router()
 protectedRouter.post('/insertSubject', subjectController.insertSubject)
-protectedRouter.post('/selectSubjectIdName', subjectController.selectSubject)
+protectedRouter.post(
+    '/selectSubjectIdName',
+    subjectController.selectSubjectIdName
+)
 module.exports = protectedRouter
--- src/pages/Subjects.js
+++ src/pages/Subjects.js
@@ -1,10 +1,45 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import '../pages/Subjects.css'
 function Subjects() {
+    const [subjects, setSubjects] = useState([])
+    useEffect(() => {
+        selectSubjects()
+    }, [])
+
+    const selectSubjects = async () => {
+        const response = await fetch(
+            'http://localhost:8080/api/subjects/selectSubjectIdName',
+            {
+                method: 'POST',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem(
+                        'accessToken'
+                    )}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    teacher_id: localStorage.getItem('userId'),
+                }),
+            }
+        )
+        const data = await response.json()
+        if (response.status === 200) {
+            setSubjects(data)
+            console.log('data was successfully inserted')
+        } else if (response.status === 500) {
+            console.log('data was not successfully inserted')
+        }
+    }
     return (
         <>
             <div className="page-container">
                 <div className="subjects-container"></div>
+                {subjects?.map((subject) => (
+                    <div className="subject-container" key={subject.subject_id}>
+                        <h1>{subject.subject_name}</h1>
+                    </div>
+                ))}
             </div>
         </>
     )
