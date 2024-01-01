9d423458e23ddd70c955c0e375ad60f2b0e0b531
RacaruCalin7 <racarucalinflorian@gmail.com>
2024-01-01 19:53:22 +0200

    api added to the note router

--- server/routes/protectedNoteRoute.js
+++ server/routes/protectedNoteRoute.js
@@ -2,4 +2,8 @@ const noteController = require('../controllers/noteController')
 const protectedRouter = require('express').Router()
 protectedRouter.post('/insertNote', noteController.insertNote)
 protectedRouter.post('/update', noteController.updateNoteTitleText)
+protectedRouter.post(
+    '/getNotesBySubjectAndUser',
+    noteController.getNotesBySubjectAndUser
+)
 module.exports = protectedRouter
