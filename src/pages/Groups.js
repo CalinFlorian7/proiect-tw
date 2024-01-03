611598a3e4c4a29e4425827081eab38d756d90e1
RacaruCalin7 <racarucalinflorian@gmail.com>
2024-01-03 15:52:44 +0200

    css tyle for the groups page header

--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import '../pages/Groups.css'
 function Groups() {
-    return <div>Groups</div>
+    return (
+        <>
+            <div className="page-container">
+                Groups
+                <div className="button-add-group">
+                    <div className="add-group">
+                        <button type="button" className="btn-add-group">
+                            Add a new group
+                        </button>
+                    </div>
+                    <div className="break"></div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Groups
