2cac3374508c02a971ea5c4bf368805e4c58aff6
RacaruCalin7 <racarucalinflorian@gmail.com>
2024-01-16 23:45:24 +0200

    css style for the button

--- src/components/Share.js
+++ src/components/Share.js
@@ -8,13 +8,21 @@ function Share({ onClose }) {
                 <div className="overlay-background" onClick={onClose}></div>
                 <div className="overlay-container">
                     <div className="overlay-content">
-                        <button
-                            className="overlay-close"
-                            type="button"
-                            onClick={onClose}
-                        >
-                            Close
-                        </button>
+                        <div className="overlay-button-close">
+                            <button
+                                className="overlay-close"
+                                type="button"
+                                onClick={onClose}
+                            >
+                                Close
+                            </button>
+                        </div>
+                        <div className="overlay-body">
+                            <h2>
+                                Sharedsabhd dsdhbsahd sdbshabdhsab
+                                dashdhsabdhbas
+                            </h2>
+                        </div>
                     </div>
                 </div>
             </div>
