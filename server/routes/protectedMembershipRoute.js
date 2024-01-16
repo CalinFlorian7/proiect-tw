e552e989b23ac75668d77cf09fd6e46a16ec6350
RacaruCalin7 <racarucalinflorian@gmail.com>
2024-01-16 19:05:33 +0200

    api addded to the membership router

--- server/routes/protectedMembershipRoute.js
+++ server/routes/protectedMembershipRoute.js
@@ -2,4 +2,8 @@ const MembershipController = require('../controllers/membershipController')
 const protectedRouter = require('express').Router()
 protectedRouter.post('/insertMember', MembershipController.insertMember)
 protectedRouter.post('/getMemberships', MembershipController.getMemberships)
+protectedRouter.post(
+    '/insertMemberByEmail',
+    MembershipController.insertMemberByEmail
+)
 module.exports = protectedRouter
